ebf91e058f830185efd23347b86210fd6c80e5e0
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-02-13 00:29:12 +0200

    Added output in metric/ imperial units for temperature

--- src/search.js
+++ src/search.js
@@ -15,10 +15,11 @@ function refreshWeather(response){
 
 function searchCity (city){
     //serach for city weather values through API
-
+    let units = getUserUnits (event);
     let APIkey="c9c17abaa3otf64ba314bf3fce705208";
+    console.log (units);
 
-    let APIUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&units${units}&key=${APIkey}`;
+    let APIUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&units=${units}&key=${APIkey}`;
     axios.get(APIUrl).then (refreshWeather);
 
 }
@@ -65,14 +66,14 @@ function getUserUnits (event) {
     let unitSelected= "";
 
     if (unitsMetric){
-        console.log(unitsMetric);
+       
         unitsSelected = "metric";
-        console.log(unitsSelected);
+        //console.log(unitsSelected);
     }
     if (unitsImperial){
-        console.log(unitsImperial);
+       
         unitsSelected = "imperial";
-        console.log(unitsSelected);
+       // console.log(unitsSelected);
         return (unitsSelected);
     }
    // console.log(unitsMetric);
